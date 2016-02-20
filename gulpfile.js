69eb9ae54926d1479b89190450d03d80b52c4c88
Joern Bernhardt <jb@campudus.com>
2016-02-20 14:53:41 +0100

    Add cleanup task
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 // lets begin!
 var gulp = require('gulp');
+var del = require('del');
 
 gulp.task('copy', function () {
   return gulp.src('src/assets/**')
     .pipe(gulp.dest('out/'));
+});
+
+gulp.task('clean', function () {
+  return del('out/');
 });
