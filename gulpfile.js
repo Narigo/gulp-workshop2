adeb069044a10cd796454dfd21b7b8abe544c54f
Joern Bernhardt <jb@campudus.com>
2016-02-20 15:11:25 +0100

    Add gulp-sass
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 // lets begin!
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var del = require('del');
 
 gulp.task('default', ['copy']);
 
+gulp.task('scss', compileScssTask);
 gulp.task('copy', copyTask);
 
 gulp.task('clean', cleanTask);
 
+function compileScssTask() {
+  return gulp.src('src/styles/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('out/'));
+}
 
 function copyTask() {
   return gulp.src('src/assets/**')
